28e7f0b13492c41d3f0a9042a9d227d158083ffe
Christian Monaghan <christian.monaghan@gmail.com>
2015-11-15 15:22:13 -0500

    initializes practice photoswipe on load

--- scripts.js
+++ scripts.js
@@ -28,10 +28,38 @@ $(function() {
     }
   };
 
-  // TODO: load photos dynamically
+  // TODO: load photos dynamically by reading directories
 
   $(window).on('scroll', onScroll);
   $(window).on('resize', resize);
   initPhotoGrid();
+
+  // Photoswipe
+  var pswpElement = document.querySelectorAll('.pswp')[0];
+
+  // build items array
+  var items = [
+      {
+          src: 'http://media2.popsugar-assets.com/files/2015/05/11/825/n/1922398/d5db8e92_shutterstock_239338216.xxxlarge_2x.jpg',
+          w: 600,
+          h: 400
+      },
+      {
+          src: 'http://localhost:8000/photography/graffiti/IMG_2142.png',
+          w: 600,
+          h: 900
+      }
+  ];
+
+  // define options (if needed)
+  var options = {
+      // optionName: 'option value'
+      // for example:
+      index: 0 // start at first slide
+  };
+
+  // Initializes and opens PhotoSwipe
+  var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+  gallery.init();
 
 });
