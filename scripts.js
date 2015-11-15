626f805827cd973a7516d5b40cf14b6eeb79929d
Christian Monaghan <christian.monaghan@gmail.com>
2015-11-15 15:36:47 -0500

    Initializes photoswipe on image click

--- scripts.js
+++ scripts.js
@@ -35,31 +35,31 @@ $(function() {
   initPhotoGrid();
 
   // Photoswipe
-  var pswpElement = document.querySelectorAll('.pswp')[0];
+  // var pswpElement = document.querySelectorAll('.pswp')[0];
 
-  // build items array
-  var items = [
-      {
-          src: 'http://media2.popsugar-assets.com/files/2015/05/11/825/n/1922398/d5db8e92_shutterstock_239338216.xxxlarge_2x.jpg',
-          w: 600,
-          h: 400
-      },
-      {
-          src: 'http://localhost:8000/photography/graffiti/IMG_2142.png',
-          w: 600,
-          h: 900
-      }
-  ];
+  // // build items array
+  // var items = [
+  //     {
+  //         src: 'http://media2.popsugar-assets.com/files/2015/05/11/825/n/1922398/d5db8e92_shutterstock_239338216.xxxlarge_2x.jpg',
+  //         w: 600,
+  //         h: 400
+  //     },
+  //     {
+  //         src: 'http://localhost:8000/photography/graffiti/IMG_2142.png',
+  //         w: 600,
+  //         h: 900
+  //     }
+  // ];
 
-  // define options (if needed)
-  var options = {
-      // optionName: 'option value'
-      // for example:
-      index: 0 // start at first slide
-  };
+  // // define options (if needed)
+  // var options = {
+  //     // optionName: 'option value'
+  //     // for example:
+  //     index: 0 // start at first slide
+  // };
 
-  // Initializes and opens PhotoSwipe
-  var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-  gallery.init();
+  // // Initializes and opens PhotoSwipe
+  // var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+  // gallery.init();
 
 });
