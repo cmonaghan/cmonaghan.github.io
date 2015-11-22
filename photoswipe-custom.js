620cd844969f5ce01d0817b294ddf3dd002af1c4
Christian Monaghan <christian.monaghan@gmail.com>
2015-11-22 17:29:50 -0500

    Cleans up some of the photoswipe-custom.js cruft

--- photoswipe-custom.js
+++ photoswipe-custom.js
@@ -1,26 +1,26 @@
 var initPhotoSwipeFromDOM = function(gallerySelector) {
 
-    // parse slide data (url, title, size ...) from DOM elements
+    // parse slide data (url, size ...) from DOM elements
     // (children of gallerySelector)
     var parseThumbnailElements = function(el) {
         var thumbElements = el.childNodes,
             numNodes = thumbElements.length,
             items = [],
-            figureEl,
+            thumbnailElement,
             linkEl,
             size,
             item;
 
         for(var i = 0; i < numNodes; i++) {
 
-            figureEl = thumbElements[i]; // <figure> element
+            thumbnailElement = thumbElements[i]; // <div> element
 
             // include only element nodes
-            if(figureEl.nodeType !== 1) {
+            if (thumbnailElement.nodeType !== 1) {
                 continue;
             }
 
-            linkEl = figureEl.children[0]; // <a> element
+            linkEl = thumbnailElement.children[0]; // <a> element
 
             size = linkEl.getAttribute('data-size').split('x');
 
@@ -31,19 +31,12 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
                 h: parseInt(size[1], 10)
             };
 
-
-
-            if(figureEl.children.length > 1) {
-                // <figcaption> content
-                item.title = figureEl.children[1].innerHTML;
-            }
-
-            if(linkEl.children.length > 0) {
+            if (linkEl.children.length > 0) {
                 // <img> thumbnail element, retrieving thumbnail url
                 item.msrc = linkEl.children[0].getAttribute('src');
             }
 
-            item.el = figureEl; // save link to element for getThumbBoundsFn
+            item.el = thumbnailElement; // save link to element for getThumbBoundsFn
             items.push(item);
         }
 
@@ -58,16 +51,16 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     // triggers when user clicks on thumbnail
     var onThumbnailsClick = function(e) {
         e = e || window.event;
-        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+        e.preventDefault(); // note: not supported by ie8
 
-        var eTarget = e.target || e.srcElement;
+        var eTarget = e.target; // note: not supported by ie8
 
         // find root element of slide
         var clickedListItem = closest(eTarget, function(el) {
             return (el.tagName && el.tagName.toUpperCase() === 'DIV');
         });
 
-        if(!clickedListItem) {
+        if (!clickedListItem) {
             return;
         }
 
@@ -80,11 +73,11 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
             index;
 
         for (var i = 0; i < numChildNodes; i++) {
-            if(childNodes[i].nodeType !== 1) {
+            if (childNodes[i].nodeType !== 1) {
                 continue;
             }
 
-            if(childNodes[i] === clickedListItem) {
+            if (childNodes[i] === clickedListItem) {
                 index = nodeIndex;
                 break;
             }
@@ -93,42 +86,14 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 
 
 
-        if(index >= 0) {
+        if (index >= 0) {
             // open PhotoSwipe if valid index found
             openPhotoSwipe( index, clickedGallery );
         }
         return false;
     };
 
-    // parse picture index and gallery index from URL (#&pid=1&gid=2)
-    var photoswipeParseHash = function() {
-        var hash = window.location.hash.substring(1),
-        params = {};
-
-        if(hash.length < 5) {
-            return params;
-        }
-
-        var vars = hash.split('&');
-        for (var i = 0; i < vars.length; i++) {
-            if(!vars[i]) {
-                continue;
-            }
-            var pair = vars[i].split('=');
-            if(pair.length < 2) {
-                continue;
-            }
-            params[pair[0]] = pair[1];
-        }
-
-        if(params.gid) {
-            params.gid = parseInt(params.gid, 10);
-        }
-
-        return params;
-    };
-
-    var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+    var openPhotoSwipe = function(index, galleryElement) {
         var pswpElement = document.querySelectorAll('.pswp')[0],
             gallery,
             options,
@@ -153,32 +118,11 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 
         };
 
-        // PhotoSwipe opened from URL
-        if(fromURL) {
-            if(options.galleryPIDs) {
-                // parse real index when custom PIDs are used
-                // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
-                for(var j = 0; j < items.length; j++) {
-                    if(items[j].pid == index) {
-                        options.index = j;
-                        break;
-                    }
-                }
-            } else {
-                // in URL indexes start from 1
-                options.index = parseInt(index, 10) - 1;
-            }
-        } else {
-            options.index = parseInt(index, 10);
-        }
+        options.index = parseInt(index, 10);
 
         // exit if index not found
-        if( isNaN(options.index) ) {
+        if ( isNaN(options.index) ) {
             return;
-        }
-
-        if(disableAnimation) {
-            options.showAnimationDuration = 0;
         }
 
         // Pass data to PhotoSwipe and initialize it
@@ -192,12 +136,6 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
     for(var i = 0, l = galleryElements.length; i < l; i++) {
         galleryElements[i].setAttribute('data-pswp-uid', i+1);
         galleryElements[i].onclick = onThumbnailsClick;
-    }
-
-    // Parse URL and open gallery if it contains #&pid=3&gid=1
-    var hashData = photoswipeParseHash();
-    if(hashData.pid && hashData.gid) {
-        openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
     }
 };
 
