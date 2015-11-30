e0d48098ff93d3a00c7d384ea41599f8f7b64b78
Christian Monaghan <christian.monaghan@gmail.com>
2015-11-29 19:41:51 -0500

    Moves soundcloud api calls into a callback

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@ $(function() {
 
   var navOffsetTop = $('.navbar').offset().top;
 
-  var initPhotoGridLayout = function() {
+  var initPhotoGridLayout = function(cb) {
     var $grid = $('.grid');
     $grid.imagesLoaded( function(){
       $grid.masonry({
         itemSelector : '.grid-item',
         gutterWidth: 7
       });
+      cb();
     });
   };
 
@@ -32,21 +33,22 @@ $(function() {
 
   $(window).on('scroll', onScroll);
   $(window).on('resize', resize);
-  initPhotoGridLayout();
+  initPhotoGridLayout(function() {
+    // loads soundcloud iframes after photo grid is rendered since they are slow
+    // piano
+    $('iframe#cleansing-rain').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/39186089&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    $('iframe#paris').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/16379629&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    $('iframe#awkward-times').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13238236&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    $('iframe#pebbles-coffee-house').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13237671&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    $('iframe#seattle').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13238314&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    // synth
+    $('iframe#castles').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13239568&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    $('iframe#you-dont-know-me-anymore').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13239636&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    $('iframe#dance-song').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/194734069&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    $('iframe#wolf-taxi').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13239291&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    $('iframe#cats-are-furry').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13239224%3Fsecret_token%3Ds-JJV3z&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+    $('iframe#synth1').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13238600&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
+  });
 
-  // loads soundcloud iframes after page load since they are slow
-  // piano
-  $('iframe#cleansing-rain').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/39186089&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  $('iframe#paris').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/16379629&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  $('iframe#awkward-times').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13238236&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  $('iframe#pebbles-coffee-house').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13237671&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  $('iframe#seattle').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13238314&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  // synth
-  $('iframe#castles').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13239568&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  $('iframe#you-dont-know-me-anymore').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13239636&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  $('iframe#dance-song').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/194734069&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  $('iframe#wolf-taxi').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13239291&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  $('iframe#cats-are-furry').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13239224%3Fsecret_token%3Ds-JJV3z&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
-  $('iframe#synth1').attr('src', 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/13238600&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false');
 
 });
